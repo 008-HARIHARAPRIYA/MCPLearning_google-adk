da28f2740cb1194b4c29f6eced7f28977673436b
008-HARIHARAPRIYA <121345379+008-HARIHARAPRIYA@users.noreply.github.com>
2026-02-22 18:44:01 +0530

    Fourth commit

--- Calculator.java
+++ Calculator.java
@@ -31,7 +31,7 @@ public class Calculator {
                 return;
         }
 
-        System.out.println("Result: " + result);
+        System.out.println("Result of Calculation is: " + result);
         scanner.close();
     }
 }
